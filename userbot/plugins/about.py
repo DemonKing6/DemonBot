847b5a74c3acf58a12c9f10e1d9438a3ce423f7c
Demon King <69576489+DemonKing6@users.noreply.github.com>
2020-08-12 14:53:45 +0530

    Update about.py

--- userbot/plugins/about.py
+++ userbot/plugins/about.py
@@ -20,8 +20,7 @@ async def _(event):
    # if input_str == "Visit this page to know more about TeleBot.":
     await event.edit("Thanks")
     animation_chars = [
-            "**TeleBot**",
-            "[More Info](https://telegra.ph/TeleBot-07-08)"
+            "**TDemonBot**"
          ]
             
 
